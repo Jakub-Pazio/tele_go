c76abd0d33fed39469da6f3fee571ef327551096
Jakub-Pazio <19jakubpazio99@gmail.com>
2023-03-12 18:04:01 +0100

    partly implemented Hamming encoding

--- hamming.go
+++ hamming.go
@@ -16,6 +16,41 @@ func CheckBit(value uint8, possition int) (bool, error) {
 	return false, nil
 }
 
-func encodeData(input byte) []byte {
+func SetBit(value *uint16, possition int) error {
+	if possition > 15 {
+		return errors.New("Wrong possition")
+	}
+	*value = *value | uint16(0x1)<<possition
+
 	return nil
+}
+
+// I count bits from right to left
+// We want to encode data on bits with numbers:
+//
+//	data: 7  6  5  4  3  2  1  0
+//	 out: 12 11 10 9  7  6  5  3
+//
+// Bits 1 2 4 8 are left for parity check; Bit 0 for whole block check
+// Other bits (13, 14, 15) are not needed and will have 0 assigned
+func EncodeData(input uint8) uint16 {
+	result := uint16(0)
+	bitMapping := [8]int{
+		0: 3,
+		1: 5,
+		2: 6,
+		3: 7,
+		4: 9,
+		5: 10,
+		6: 11,
+		7: 12,
+	}
+	for i, v := range bitMapping {
+		isSet, _ := CheckBit(input, i)
+		if isSet == true {
+			SetBit(&result, v)
+		}
+	}
+
+	return result
 }
--- hamming_test.go
+++ hamming_test.go
@@ -32,5 +32,40 @@ func TestCheckBit(t *testing.T) {
 			t.Error("error not expected")
 		}
 	})
+}
 
+func TestSetBit(t *testing.T) {
+	t.Run("Set bit when bit is 0", func(t *testing.T) {
+		firstBitoff := uint16(0xFFE)
+
+		want := uint(0xFFF)
+		SetBit(&firstBitoff, 0)
+
+		if uint(firstBitoff) != want {
+			t.Errorf("wanted %d got %d", want, firstBitoff)
+		}
+	})
+	t.Run("Set bit when bit is 1", func(t *testing.T) {
+		firstBiton := uint16(0xDDB)
+
+		want := uint16(0xDDB)
+		SetBit(&firstBiton, 0)
+
+		if firstBiton != want {
+			t.Errorf("wanted %d got %d", want, firstBiton)
+		}
+	})
+}
+
+func TestEncode(t *testing.T) {
+	t.Run("Test basic Hamming encoding", func(t *testing.T) {
+		valueToEncode := uint8(0xF7)
+
+		want := uint16(0x1E68)
+		got := EncodeData(valueToEncode)
+
+		if got != want {
+			t.Errorf("wanted %d got %d", want, got)
+		}
+	})
 }
